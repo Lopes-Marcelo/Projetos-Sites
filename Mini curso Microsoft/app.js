6773678f1290052fff21b92f49f459668bc78ce5
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-10 23:14:17 -0400

    ultimas

--- Mini curso Microsoft/app.js
+++ Mini curso Microsoft/app.js
@@ -1,1 +1,18 @@
-'use strict';
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Botão";
+    }
+    else {
+        this.textContent = "Botão";
+    }
+
+    console.log('current class name: ' + className);
+
+});
